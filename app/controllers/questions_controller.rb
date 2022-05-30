fc85054d9db625f05f42e67eac28ba21ac74717f
cd <charlydubostbp@hotmail.fr>
2022-05-30 14:40:56 +0200

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    toto = params[:answer] || ""
+
+    if toto == 'I am going to work'
+      @answer = 'Great'
+    elsif toto.chars.last == '?'
+      @answer = 'Silly question, get dressed and go to work!.'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
